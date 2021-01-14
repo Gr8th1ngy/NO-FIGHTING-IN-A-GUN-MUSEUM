68e0fda14709fa73376c6db00e94cc0a07b7e49f
Gr8th1ngy <anhhuyapmops@gmail.com>
2021-01-13 21:49:37 -0500

    Allow movement controls

---
+++ Tom Francis tutorial/Assets/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    public float speed;
+
+    //// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float maxDistanceToMove = speed * Time.deltaTime;
+
+        // Vertical movement
+        transform.position += Vector3.forward * Input.GetAxis("Vertical") * maxDistanceToMove;
+        transform.position += Vector3.right * Input.GetAxis("Horizontal") * maxDistanceToMove;
+    }
+}
